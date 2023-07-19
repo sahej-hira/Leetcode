b59b178a96df376d16b5da1cbd9aaf391b4d201a
Sahej <89015087+shark-lamp@users.noreply.github.com>
2023-07-19 17:10:22 +0530

    Time: 23 ms (31.34%) | Memory: 13.4 MB (56.18%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        hash={")":"(","]":"[","}":"{"}
+        stk=[]
+         
+        for i in s:
+            if i in hash:
+                if stk and stk[-1]==hash[i]:
+                    print("popped")
+                    stk.pop()
+                else:
+                    return False
+            else:
+                print("append")
+                stk.append(i)
+        if stk == []:
+            return True
+        print(stk)
+        return False 
