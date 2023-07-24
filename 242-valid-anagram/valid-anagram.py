6f45ecf3276e4279a290edf52760a9c173fa3f12
Sahej <89015087+shark-lamp@users.noreply.github.com>
2023-07-24 23:48:04 +0530

    Time: 72 ms (37.75%) | Memory: 16.8 MB (65.07%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+#counter of char in hashmap & compare
+        if len(s)!=len(t):
+            return False
+        counts,countt={},{}
+        for i in range(len(s)):
+            counts[s[i]]=1+counts.get(s[i],0)
+            countt[t[i]]=1+countt.get(t[i],0)
+        for c in counts:
+            if counts[c]!=countt.get(c,0):
+                return False
+        return True
+
+#sort & compare
+#counter()
+#for loop
