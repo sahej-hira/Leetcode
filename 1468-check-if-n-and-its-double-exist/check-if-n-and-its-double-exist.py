a3c9e9074fc039221efe0c8ac1e6f6d1cc45cc64
Sahej <89015087+shark-lamp@users.noreply.github.com>
2023-07-29 15:24:31 +0530

    Time: 58 ms (96.27%) | Memory: 16.4 MB (76.24%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -4,9 +4,9 @@ class Solution:
         for i,v in enumerate(arr):
             double=v*2
             org=double%2#original
-            print(prevmap)
+            #print(prevmap)
             if v*2 in prevmap or v%2==0 and v/2 in prevmap: #v%2 in prevmap:not right
-                print(v,(v*2),v/2)
+                #print(v,(v*2),v/2)
                 return True
             prevmap[v]=i
         return False
