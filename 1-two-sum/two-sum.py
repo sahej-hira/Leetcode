a86ac1dd2c565b56c322c57dc73acadcebd24478
Sahej <89015087+sahej-hira@users.noreply.github.com>
2025-10-18 14:59:16 +0530

    Time: 3026 ms (5.51%) | Memory: 18.2 MB (95.94%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,32 +1,16 @@
-from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = defaultdict()
+        one = 0
         
-        for index, value in enumerate(nums):
-            diff = target - value
-            if diff in hashmap:
-                break
-            else:
-                hashmap[value] = index
-        
-        return hashmap.get(diff), index
-        
-        
-        
-        
-        '''
-        from collections import defaultdict
 
-        # Creating a hashmap using defaultdict
-        hashmap = defaultdict(int)
+        while one < len(nums):
+            two = one + 1
 
-        # Adding key-value pairs to the hashmap
-        hashmap["key1"] = 10
-        hashmap["key2"] = 20
-        hashmap["key3"] = 30
-
-        # Accessing values in the hashmap
-        print(hashmap["key2"])  # Output: 20
-        '''
-        
+            while two < len(nums):
+                #print(one,two)
+                if nums[one] + nums[two] == target:
+                    return [one, two]
+                two += 1
+            one += 1
+        #print("all done")
+        return [one,two]
